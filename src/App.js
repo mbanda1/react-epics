97b36028873beac8ffdce4b4f4e04de937b0f059
Nixon <mbandanixon@gmail.com>
2022-11-11 18:51:04 +0300

    Home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { Button, Container, Grid, Icon, Segment } from 'semantic-ui-react';
 import './App.css';
+import DisplayBalance from './components/displayBalance';
+import MainHeader from './components/header';
+import DisplayBalances from './components/displayBalances';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import EntryLine from './components/entryLine';
+import EntryLines from './components/entryLines';
+import ModalEdit from './components/addNew/modalEdit';
 
 function App() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [total, setTotal] = useState(0);
+  const [expenseTotal, setExpenseTotal] = useState(0);
+  const [incomeTotal, setIncomeTotal] = useState(0);
+  const [updated, setUpdated] = useState();
+  const entries = useSelector((state) => state.entries);
+  const { isOpen, entry } = useSelector((state) => state.modal);
+
+    useEffect(() => {
+      dispatch({ type:  'GET_ENTRIES' });
+    }, [dispatch, updated]);
+
+    const data = entries?.data || []
+console.log({isOpen})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+     <MainHeader title='The Budget Calculater App' />
+     <DisplayBalance title='Your Balance:' value={total} size='small' />
+     <Button onClick={() => navigate('/add')}>Add New</Button>
+     <DisplayBalances expenseTotal={expenseTotal} incomeTotal={incomeTotal} />
+
+     <MainHeader title='History' type='h3' />
+     <EntryLines entries={data} />
+
+     <ModalEdit isOpen={isOpen} {...entry} />
+
+    </Container>
   );
 }
 
