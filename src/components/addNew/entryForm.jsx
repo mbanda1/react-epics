c07adce5e3e558f16bd2e7afd3b54bd74e251f7f
Nixon <mbandanixon@gmail.com>
2023-01-16 09:13:00 +0300

    Rename to jsx

---
+++ src/components/addNew/entryForm.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import * as PropTypes from 'prop-types'
+import { yupResolver } from '@hookform/resolvers/yup'
+import './style.css'
+
+const validationSchema = yup.object({
+  value: yup.number().typeError('Value is equired'),
+  description: yup.string().required('Required'),
+})
+
+export default function App({ addEntryRedux }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  })
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit((data) => addEntryRedux(data))}>
+        <label htmlFor="fname">Value</label>
+        <input type="number" {...register('value')} placeholder="999" />
+        {errors.value && <p>{errors.value?.message}</p>}
+
+        <br />
+        <label htmlFor="fname">Description</label>
+        <input type="text" {...register('description')} placeholder="Tea" />
+        {errors.description && <p>{errors.description?.message}</p>}
+        <br />
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
+
+App.propTypes = {
+  addEntryRedux: PropTypes.func,
+}
---
+++ src/components/addNew/newEntry.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { Button, Icon } from 'semantic-ui-react'
+import EntryForm from './entryForm'
+import { v4 as uuidv4 } from 'uuid'
+// import useEntryDetails from '../hooks/useEntryDetails';
+import MainHeader from '../header'
+import { useNavigate } from 'react-router-dom'
+
+function NewEntryForm() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const addEntryRedux = (payload) => {
+    dispatch({
+      type: 'ADD_ENTRY_RESULT',
+      payload: {
+        id: uuidv4(),
+        value: payload.value,
+        description: payload.description,
+        notification: 'Inserted',
+      },
+    })
+    navigate('/')
+  }
+
+  return (
+    <>
+      <MainHeader title="Add new transaction" type="h3" />
+      <EntryForm addEntryRedux={addEntryRedux} />
+
+      <Button icon labelPosition="left" onClick={() => navigate('/')}>
+        Back
+        <Icon name="left arrow" />
+      </Button>
+    </>
+  )
+}
+
+export default NewEntryForm
